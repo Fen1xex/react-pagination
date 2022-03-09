7e706fd79d7cd6a11f35c446293a0e88742eaab2
KIRILL F <tiltlessman@gmail.com>
2022-03-09 15:58:41 +0300

    fetch data with axios, with hooks useState,useEffect

--- src/context.js
+++ src/context.js
@@ -1,10 +1,31 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
 const AppContext = React.createContext()
 const url = 'https://api.github.com/users/bradtraversy/followers?per_page=100'
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [isLoading, setIsLoading] = useState(false)
+  const [followers, setFollowers] = useState([])
+
+  const fetchFollowers = async () => {
+    setIsLoading(true)
+    await axios(url)
+      .then(({ data }) => {
+        setFollowers(data)
+      })
+      .catch((error) => console.log(error))
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    fetchFollowers()
+  }, [])
+
+  return (
+    <AppContext.Provider value={{ followers, isLoading }}>
+      {children}
+    </AppContext.Provider>
+  )
 }
 
 export const useGlobalContext = () => {
